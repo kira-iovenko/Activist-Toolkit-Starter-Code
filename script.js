858df3af082f852823dfcc2bcff643eccc1f1aad
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2024-08-11 19:54:47 +0000

    Add the <label>, <input>, and <button> elements to my index.html file, create variables to store the <p> element where the message will display and the <button> element in my script.js file, attach an event listener to the variable storing the <button> element, define the function I named inside the event listener.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /* .js files add interaction to your website */
+var displayName = document.getElementById("message");
+var nameButton = document.getElementById("nameButton");
+
+nameButton.addEventListener("click", displayMessage);
+
+function displayMessage() {
+  var name = document.getElementById("yourName").value;
+  displayName.innerHTML =
+    name +
+    ", your mental health is just as important as your physical health. Don't be afraid to speak up about it.";
+}
