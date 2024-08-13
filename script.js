cd760df769ed061e98f57c31b77d558121f9d4bf
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2024-08-13 18:30:49 +0000

    Create a section to generate quotes, add a button with id "quoteButton", create an array to store the quotes, add a <p> element with an id attribute to display the text from the array

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /* .js files add interaction to your website */
+
+/*Display a message after a button is clicked*/
 var displayName = document.getElementById("message");
 var nameButton = document.getElementById("nameButton");
 
@@ -10,3 +12,11 @@ function displayMessage() {
     name +
     ", your mental health is just as important as your physical health. Don't be afraid to speak up about it.";
 }
+
+/*Generate a quote from an array*/
+var quoteList = [
+  "The strongest people are not those who show strength in front of the world but those who fight and win battles that others do not know anything about. ― Jonathan Harnisch",
+  "The first step toward change is awareness. The second step is acceptance. — Nathaniel Branden",
+  "Awareness is like the sun. When it shines on things, they are transformed. — Thich Nhat Hanh",
+  "What mental health needs is more sunlight, more candor, and more unashamed conversation. — Glenn Close",
+];
